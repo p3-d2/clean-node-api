93380e25f2e850fd92e9c2fcdc82e2b7ec91dd32
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-25 14:12:08 -0300

    move erros to a custom folder

--- presentation/routes/login-router.unit.js
+++ presentation/routes/login-router.unit.js
@@ -1,8 +1,5 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../helpers/missing-param-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
-const ServerError = require('../helpers/server-error')
+const { ServerError, UnauthorizedError, InvalidParamError, MissingParamError } = require('../errors')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
