ff8a6ac59603c921b79538f4affb18ed84070fe6
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-27 12:40:09 -0300

    move mongodb setup to a separate file

---
+++ src/infra/helpers/mongo-helper.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb')
+
+module.exports = {
+  async connect (url, dbName) {
+    this.url = url
+    this.dbName = dbName
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    this.db = this.client.db(dbName)
+  },
+
+  async disconnect () {
+    await this.client.close()
+  }
+}
--- src/infra/repositories/load-user-by-email-repository.unit.js
+++ src/infra/repositories/load-user-by-email-repository.unit.js
@@ -1,7 +1,7 @@
-const { MongoClient } = require('mongodb')
+const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 
-let client, db
+let db
 
 const makeSut = () => {
   const userModel = db.collection('users')
@@ -11,11 +11,8 @@ const makeSut = () => {
 
 describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
-    client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    db = client.db()
+    await MongoHelper.connect(process.env.MONGO_URL)
+    db = MongoHelper.db
   })
 
   beforeEach(async () => {
@@ -23,7 +20,7 @@ describe('LoadUserByEmail Repository', () => {
   })
 
   afterAll(async () => {
-    await client.close()
+    await MongoHelper.disconnect()
   })
 
   test('Should return null if no user is found', async () => {
