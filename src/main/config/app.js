f5f831f83520bb24c1c8eb9dedf68d2a2281c861
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-27 15:22:36 -0300

    disable x-powered-by header

---
+++ src/main/config/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const setupApp = require('./setup')
+
+const app = express()
+
+setupApp(app)
+
+module.exports = app
--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,3 @@
-const express = require('express')
-const app = express()
+const app = require('./config/app')
 
 app.listen(3000, () => console.log('Server running'))
