211995a470653d15288ef8b0421a3ba16f25fb48
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-27 16:51:58 -0300

    add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const setupApp = require('./setup')
+const setupRoutes = require('./routes')
 
 const app = express()
 
 setupApp(app)
+setupRoutes(app)
 
 module.exports = app
