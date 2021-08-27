4b4e034531a38db76ea74938495ac62de35c6a19
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-27 15:27:25 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((_, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
