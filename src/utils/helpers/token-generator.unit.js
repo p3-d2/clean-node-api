fd8c1875205c1cdd057f4ea788eddeb8701f382d
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-28 14:18:55 -0300

    improve id to payload in tokenGenerator

--- src/utils/helpers/token-generator.unit.js
+++ src/utils/helpers/token-generator.unit.js
@@ -1,7 +1,7 @@
 jest.mock('jsonwebtoken', () => ({
   token: 'any_token',
-  sign (id, secret) {
-    this.id = id
+  sign (payload, secret) {
+    this.payload = payload
     this.secret = secret
     return this.token
   }
@@ -30,8 +30,10 @@ describe('Token Generator', () => {
   test('Should call JWT with correct values', async () => {
     const sut = makeSut()
     await sut.generate('any_id')
-    expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
+    expect(jwt.payload).toEqual({
+      _id: 'any_id'
+    })
   })
 
   test('Should throw if no secret is provided', async () => {
